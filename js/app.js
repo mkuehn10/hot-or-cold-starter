42d51452c9ddf18e81625df946f38766005a84e7
Michael Kuehn <mkuehn10@gmail.com>
2016-07-13 21:12:43 -0400

    implement newgame function

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 
 $(document).ready(function(){
-	
+
+    'use strict';
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -11,6 +13,23 @@ $(document).ready(function(){
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
+
+    var numberToGuess;
+
+    function newGame() {
+        $('#feedback').text('Make your Guess!');
+        $('#count').text('0');
+        $('#guessList').text('');
+        $('#userGuess').val('');
+        numberToGuess = Math.floor(Math.random() * (100 - 1)) + 1;
+        console.log(numberToGuess);
+    }
+
+    newGame();
+
+    $('.new').click(newGame);
+
+
 
 });
 
